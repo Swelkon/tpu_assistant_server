b89d237be787a96d92397688cec5088f91b091ec
Belig Chimitov <belig.ch111@gmail.com>
2021-12-15 21:17:11 +0700

    added ApiFAQ

--- src/questions/questions.service.ts
+++ src/questions/questions.service.ts
@@ -3,16 +3,35 @@ import { InjectModel } from '@nestjs/mongoose'
 import { Model } from 'mongoose'
 import { CreateQuestionDto } from './dtos/create-question.dto'
 import { Question, QuestionDocument } from './schemas/question.schema'
+import {ApiFAQ} from "../TPUApi/ApiFAQ";
+import {HttpService} from "@nestjs/axios";
+import {ServerResponse} from "../model/ServerResponse";
 
 @Injectable()
 export class QuestionsService {
+
+    private apiFAQ: ApiFAQ
+
     constructor(
         @InjectModel(Question.name)
         private readonly _questionModel: Model<QuestionDocument>,
-    ) {}
+        private httpService: HttpService
+    ) {
+        this.apiFAQ = new ApiFAQ(httpService)
+    }
 
     async createQuestion(createQuestionDto: CreateQuestionDto): Promise<void> {
         const question = await this._questionModel.create(createQuestionDto)
         await question.save()
+    }
+
+    async getFAQ(question: string) {
+
+        const answer = await this.apiFAQ.getAnswerFAQ(question)
+
+        if (!answer){
+            return ServerResponse.sendServerFail()
+        }
+        return ServerResponse.sendFAQAnswer(answer);
     }
 }
